84f43a1287ecbe2e7c4627ef4e51d45e0a8a51be
Gabriel <gabriel.eugenio@titanium.solutions>
2026-03-21 19:53:18 +0000

    feat: implement dashboard and side menu components, update routing and layout

--- titanium-portal/src/app/app.routes.ts
+++ titanium-portal/src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' }, // default redirect
     { path: 'login', component: LoginComponent },
-    {path: 'menu', component: MenuComponent},
+    { path: 'dashboard', component: DashboardComponent }, // example protected route
+    //{path: 'menu', component: MenuComponent},
     //{ path: 'profile', component: ProfileComponent },// example protected route
      // optional wildcard (404 fallback)
     { path: '**', redirectTo: 'login' }
--- titanium-portal/src/app/app.ts
+++ titanium-portal/src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Router, RouterOutlet } from '@angular/router';
+import {MenuComponent} from './menu/menu.component';
+import { NgIf } from '@angular/common';
+import { SideMenuComponent } from "./side-menu/side-menu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, NgIf, SideMenuComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('titanium-portal');
+   constructor(public router: Router) {}
+
+  isLoginPage(): boolean {
+    console.log('Current URL:', this.router.url);
+    return this.router.url === '/login';
+  }
 }
